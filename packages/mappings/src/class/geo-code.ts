d3e63cd7d4e7b08170331373b5aa79097d6f0d03
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-09 12:55:58 +0100

    GEO-CODE | CLASS

--- packages/mappings/src/class/geo-code.ts
+++ packages/mappings/src/class/geo-code.ts
@@ -1,1 +1,56 @@
-export class heremaps_Geocode {}
+import * as rp from "request-promise";
+
+export class heremaps_Geocode {
+    url?: string;
+    options: {
+        headers?: {
+            [key: string]: string;
+        };
+        json: boolean;
+    };
+    appId: string;
+    appCode: string;
+    constructor() {
+        this.url = process.env.HERE_MAP_BASE_URL as string;
+        this.options = {
+            json: true
+        };
+        this.appId = process.env.HERE_MAPS_app_id as string;
+        this.appCode = process.env.HERE_MAPS_app_code as string;
+    }
+
+    private urlConstructor(
+        params: HereMaps.gcpa,
+        model: HereMaps.geocode_models
+    ) {
+        if (model === "pa") {
+            return `${this.url}?app_id=${this.appId}&app_code=${
+                this.appCode
+            }&country=${params.country}&housenumber=${
+                params.housenumber
+            }&city=${params.city}&street=${params.street}`;
+        }
+
+        if (model === "ffa") {
+        }
+    }
+
+    /**
+     * gc_Partial_Address
+     */
+    public async gc_Partial_Address(params: HereMaps.gcpa) {
+        const uri = this.urlConstructor(params, "pa");
+        let result = await rp.get(uri);
+
+        try {
+            return { ok: true, result };
+        } catch (error) {
+            return { ok: false };
+        }
+    }
+
+    /**
+     * gc_ff_Address
+     */
+    public gc_ff_Address(freeForm: HereMaps.gffa) {}
+}
---
+++ packages/mappings/src/types/here.maps.d.ts
@@ -0,0 +1,13 @@
+declare namespace HereMaps {
+    interface gcpa {
+        country: string;
+        housenumber: string;
+        city: string;
+        street: string;
+        fft?: string;
+    }
+
+    type gffa = string;
+
+    type geocode_models = "pa" | "ffa";
+}
