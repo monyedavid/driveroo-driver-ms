973e0a6f1718b84dea549cae919951a26d1aa793
rasheeddavid <griffinc317@gmail.com>
2019-09-03 09:58:49 +0100

    Google | MAPs | :)

--- packages/mappings/src/types/google.maps.d.ts
+++ packages/mappings/src/types/google.maps.d.ts
@@ -27,4 +27,59 @@ declare namespace GoogleMaps {
         predictions: [auto_Complete_Params_predictions];
         status: string;
     }
+
+    type lat_long = {
+        lat: number;
+        lng: number;
+    };
+
+    interface place_details_Params_result {
+        address_components: [
+            { long_name: string; short_name: string; types: [string] }
+        ];
+        adr_address: string;
+        geometry: {
+            location: lat_long;
+            viewport: { northeast: lat_long; southwest: lat_long };
+        };
+        icon: string;
+        id: string;
+        name: string;
+        photos: [
+            {
+                height: number;
+                html_attributions: [string];
+                photo_reference: string;
+                width: number;
+            }
+        ];
+        reference: string;
+        scope: string;
+        types: [string];
+        url: string;
+        utc_offset: number;
+        vicinity: string;
+    }
+
+    interface place_details_Params {
+        html_attributions: [any];
+        result: place_details_Params_result;
+        status: string;
+    }
+
+    interface place_Search {
+        candidates: [
+            {
+                place_id: string;
+            }
+        ];
+        status: string;
+    }
+
+    interface place_nearby_Search {
+        error_message: string;
+        html_attributions: [];
+        results: [];
+        status: string;
+    }
 }
